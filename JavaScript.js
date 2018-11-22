4084ed32bc14891a3765ef725ceab7ed60649999
José Adolfo <jmicalco@gmail.com>
2018-11-22 17:07:31 -0600

    modifications

--- JavaScript.js
+++ JavaScript.js
@@ -1,10 +1,15 @@
 //JavaScript para pagina de Proyecto
 //Codigo para inizializar los contenedores collapsible
-/document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems, options);
-});
+
 
 $(document).ready(function(){
   $('.collapsible').collapsible();
+});
+
+ $(document).ready(function(){
+$('.modal').modal();
+});
+
+$(document).ready(function(){
+$('.materialboxed').materialbox();
 });
